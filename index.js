a26463b1984579556eea1bbb13e2cd43878fc53e
Daniel Bretoi <daniel@bretoi.com>
2020-04-30 14:14:15 -0700

    remove unused stuff for testing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const Pkg = require(`${process.cwd()}/package.json`);
+const Pkg = require('./package.json');
 const CP = require('child_process');
 const CamelCase = require('camelcase');
 
@@ -11,18 +11,26 @@ const requires = function (obj) {
 
 const main = function () {
 
+	console.log(`CLIn v${Pkg.version}`);
+	let Load = '';
+	try {
+		Load = require(`${process.cwd()}/package.json`);
+	} catch (e) {
+		console.log("A package.json must be in the same folder to load dependencies");
+	}
 	const reqs = [];
-	reqs.push(...requires(Pkg.dependencies));
-	reqs.push(...requires(Pkg.devDependencies));
+	reqs.push(...requires(Load.dependencies));
+	reqs.push(...requires(Load.devDependencies));
 	const final = reqs.map((i) => {
 		
 		const name = CamelCase(i.replace(/[@/]/g, '-'));
 		if (i !== name) {
-			console.log(`'${i}' as ${name}`);
+			console.log(`  '${i}' as ${name}`);
 		}
 		return `try { global.${name} = require("${i}") } catch (e) { console.log("Failed to load '${i}'") }`;
 	}).join(';');
 
+	console.log("");
 	const res = CP.spawnSync('node', ['-i', '-e', final], { stdio: 'inherit' });
 };
 
