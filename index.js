ebb0ac61b1b06e3de0165613b1626d7c5a0c5d07
Daniel Bretoi <daniel@otherware.org>
2022-07-01 21:15:33 -0700

    updates for colors and things

--- index.js
+++ index.js
@@ -2,36 +2,62 @@
 const Pkg = require('./package.json');
 const CP = require('child_process');
 const CamelCase = require('camelcase');
+const Chalk = require('chalk');
 
 const requires = function (obj) {
-	if (!obj) return [];
+    if (!obj) return [];
 
-	return Object.keys(obj);
+    return Object.keys(obj);
+};
+
+const clicmd = function (name, lib) {
+    return `try { global.${name} = require("${lib}") } catch (e) { console.log("Failed to load '${lib}' as ${name}") };\n`;
+};
+
+const name_for = function (lib) {
+    return CamelCase(lib.replace(/[@/]/g, '-'));
+};
+
+const display = function (lib, name, longestString) {
+    console.log(
+        '',
+        Chalk.blue(lib.padEnd(longestString + 4)),
+        'as ',
+        Chalk.green(name)
+    );
 };
 
 const main = function () {
+    console.log(Chalk.bold.underline.yellow(`CLIn v${Pkg.version}`));
+    let Load = '';
 
-	console.log(`CLIn v${Pkg.version}`);
-	let Load = '';
-	try {
-		Load = require(`${process.cwd()}/package.json`);
-	} catch (e) {
-		console.log("A package.json must be in the same folder to load dependencies");
-	}
-	const reqs = [];
-	reqs.push(...requires(Load.dependencies));
-	reqs.push(...requires(Load.devDependencies));
-	const final = reqs.map((i) => {
-		
-		const name = CamelCase(i.replace(/[@/]/g, '-'));
-		if (i !== name) {
-			console.log(`  '${i}' as ${name}`);
-		}
-		return `try { global.${name} = require("${i}") } catch (e) { console.log("Failed to load '${i}'") }`;
-	}).join(';');
+    try {
+        Load = require(`${process.cwd()}/package.json`);
+    } catch (e) {
+        console.log(
+            'A package.json must be in the same folder to load dependencies'
+        );
+    }
 
-	console.log("");
-	const res = CP.spawnSync('node', ['-i', '-e', final], { stdio: 'inherit' });
+    const reqs = [];
+    reqs.push(...requires(Load.dependencies));
+    reqs.push(...requires(Load.devDependencies));
+
+    let longestString = reqs.reduce((p, c) =>
+        name_for(p).length > name_for(c).length ? name_for(p) : name_for(c)
+    ).length;
+
+    const final =
+        reqs
+            .map((lib) => {
+                const name = name_for(lib);
+                display(lib, name, longestString);
+                return clicmd(name, lib);
+            })
+            .join(';') + clicmd('main', './' + Load.main);
+
+    console.log('');
+    const res = CP.spawnSync('node', ['-i', '-e', final], { stdio: 'inherit' });
 };
 
 main();
